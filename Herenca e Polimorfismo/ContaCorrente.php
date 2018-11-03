05acb27c07ae21505eff614460f7f16f17f66dcf
Kauan Amorim <kauanamorim88@gmail.com>
2018-11-03 18:40:45 -0300

    (Portuguese) Adicionando pratica sobre heranca e polimorfismo - (English) Adding practice on inheritance and polymorphism

---
+++ Herenca e Polimorfismo/ContaCorrente.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "Conta.php";
+
+class ContaCorrente extends Conta {
+
+    protected $limite;
+
+    public function __construct($agencia, $conta, $saldo, $limite){
+        parent::__construct($agencia, $conta, $saldo);
+        $this->limite = $limite;
+    }
+
+    /**
+     * This method decreases the $saldo (English).
+     * Esse método decrementa a $saldo (Portuguese).
+     * @access public
+     * @param Number $quantia;
+     * @return Boolean
+     */
+    public function retirar($quantia){
+
+        if($this->saldo + $this->limite >= $quantia){
+            $this->saldo -= $quantia;
+        } else {
+            return false;
+        }
+        
+        return true;
+
+    }
+
+}
