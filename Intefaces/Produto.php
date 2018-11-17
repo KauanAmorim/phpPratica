eca4b2c6f78a478815cee0d6a2ec12b8d5775a19
Kauan Amorim <kauanamorim88@gmail.com>
2018-11-17 17:03:13 -0200

    Imprementacao de conceitos sobre Interfaces (Sem documentacao)

---
+++ Intefaces/Produto.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "OrcavelInterface.php";
+
+class Produto implements OrcavelInterface {
+
+    private $descricao;
+    private $estoque;
+    private $preco;
+
+    public function __construct($descricao, $estoque, $preco){
+        $this->descricao = $descricao;
+        $this->estoque = $estoque;
+        $this->preco = $preco;
+    }
+
+    public function getPreco(){
+        return $this->preco;
+    }
+
+}
